becf7f34e539aad86f6669abe898869f9f1fb3f7
hlebychek <hleb21901@gmail.com>
2026-05-05 20:41:13 +0300

    Подключение пользователей и WebSocket

---
+++ client/realtime-board-frontend/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <BoardList />
+  </div>
+</template>
+
+<script>
+import BoardList from './components/BoardList.vue'
+
+export default {
+  name: 'App',
+  components: {
+    BoardList
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  font-family: Arial, sans-serif;
+  width: 100%;
+  height: 100%;
+
+}
+
+#app {
+    width: 100%;
+  min-height: 100vh;
+}
+
+</style>
