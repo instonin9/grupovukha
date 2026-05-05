9ffa14a949f8955f7e85a8c77b739a3dc08d749b
hlebychek <hleb21901@gmail.com>
2026-05-05 15:09:22 +0300

    создание кнопки

---
+++ client/board-frontend/src/components/BoardView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="board-view">
+    <h1>Доска {{ $route.params.id }}</h1>
+    <p>доска </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BoardView'
+};
+</script>
---
+++ client/board-frontend/src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<template>
+  <CreateBoard />
+</template>
+
+<script>
+import CreateBoard from '../components/CreateBoard.vue';
+
+export default {
+  name: 'HomeView',
+  components: {
+    CreateBoard
+  }
+};
+</script>
